873f18b262a5db25a72d9f7c9f4fbf5a4af28dc9
Carlo Macor <carlo.macor@libero.it>
2018-02-21 10:35:20 +0100

    changed only application file

--- MvvmBitcoin/app/src/main/java/com/macormap/mvvmbitcoin/StockApplication.java
+++ MvvmBitcoin/app/src/main/java/com/macormap/mvvmbitcoin/StockApplication.java
@@ -7,6 +7,29 @@ import com.macormap.mvvmbitcoin.di.AppComponent;
 import com.macormap.mvvmbitcoin.di.AppModule;
 import com.macormap.mvvmbitcoin.di.DaggerAppComponent;
 
+//  Carlo Macor  :  February 2018
+
+/**
+ *    AppComponent mAppComponent;   is created using Dagger
+ *    Note that we write            : DaggerAppComponent.builder().
+ *    because we have a unit named  :       AppComponent
+ *    if We have a unit named NetComponent We'll write DaggerNetComponent.builder().
+ *
+ *    Also  .appModule(new AppModule(this)) becaouse we start constructor of AppModule.java
+ *    that need Application parameter (this application)
+ *
+ *      it's possible that we have more modules to init so we have more line like this one
+ *      .netModule(new NetModule(parameter to init))
+ *      .someModule(new SomeModule() )
+ *
+ *      so in MainActivity we can get reference to this AppComponent just created.
+ *      using the form : ((StockApplication) getApplication()).getAppComponent().inject(this);
+ *      and activate dagger from that moment.
+ *
+ *      see comments in AppComponent.java
+ */
+
+
 public class StockApplication extends Application {
 
     AppComponent mAppComponent;
